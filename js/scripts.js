51bca54f1f7e3289a63ff06e6d026f14d8dc730c
StefanDellamore <dellamoreste.info@gmail.com>
2023-12-09 17:24:35 +0100

    Impostazione array per img e definizione dei bottoni

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,47 @@ const imgQuattro = document.querySelector ('.card').innerHTML = '<img src="img/0
 console.log ('imgQuattro', imgQuattro, typeof imgQuattro)
 const imgCinque = document.querySelector ('.card').innerHTML = '<img src="img/05.webp" alt="05">';
 console.log ('imgCinque', imgCinque, typeof imgCinque)
+
+//impostazione array per immagini
+const imgList = [ 
+    ` ${imgUno} `,
+    ` ${imgDue} `,
+    ` ${imgTre} `,
+    ` ${imgQuattro} `,
+    ` ${imgCinque} ` 
+     ];
+
+     console.log ('imgList', imgList, typeof imgList)
+
+document.querySelector('.card').innerHTML = (imgList[0]);
+
+let contatore = 0
+
+//definire bottoni in variabili
+
+const buttonForward = document.getElementById ('forward');
+console.log ('buttonForward', buttonForward, typeof buttonForward)
+const buttonBack = document.getElementById ('back');
+console.log ('buttonBack', buttonBack, typeof buttonBack)
+
+
+
+
+buttonForward.addEventListener('click', function() {
+    
+    if (contatore < imgList.length -1){
+        contatore += 1;
+        console.log ('contatore', contatore, typeof contatore);
+        document.querySelector('.card').innerHTML = imgList[contatore];
+    }
+    
+})
+
+buttonBack.addEventListener ('click', function() {
+    
+    if ((contatore !== imgList.lenght) && (contatore !== 0)){
+        contatore -= 1;
+        console.log ('contatore', contatore, typeof contatore);
+        document.querySelector ('.card').innerHTML = (imgList[contatore]);
+    }
+})
