c42d76c16909ed4a2be52b1498652a2f5080e7c1
StefanDellamore <dellamoreste.info@gmail.com>
2023-12-09 17:23:36 +0100

    Definizioni immagini in variabili

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//definire immagini in variabili
+
+const imgUno = document.querySelector ('.card').innerHTML = '<img src="img/01.webp" alt="01">';
+console.log ('imgUno', imgUno, typeof imgUno)
+const imgDue = document.querySelector ('.card').innerHTML = '<img src="img/02.webp" alt="02">';
+console.log ('imDue', imgDue, typeof imgDue)
+const imgTre = document.querySelector ('.card').innerHTML = '<img src="img/03.webp" alt="03">';
+console.log ('imgTre', imgTre, typeof imgTre)
+const imgQuattro = document.querySelector ('.card').innerHTML = '<img src="img/04.webp" alt="04">';
+console.log ('imgQuattro', imgQuattro, typeof imgQuattro)
+const imgCinque = document.querySelector ('.card').innerHTML = '<img src="img/05.webp" alt="05">';
+console.log ('imgCinque', imgCinque, typeof imgCinque)
